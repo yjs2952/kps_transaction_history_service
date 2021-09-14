ba7926562abb0a353dfb8550f5bfef9caca8b279
yjs2952 <yjs2952@naver.com>
2021-09-14 11:44:43 +0900

    Initial commit

---
+++ src/main/java/com/kpsec/KpsTransactionHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kpsec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpsTransactionHistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KpsTransactionHistoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kpsec/KpsTransactionHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpsec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpsTransactionHistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
